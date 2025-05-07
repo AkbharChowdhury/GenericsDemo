cd5355c847c321921dd9d9f7a54ca730a8baf574
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-05-07 09:10:00 +0100

    m

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from pydantic import BaseModel, conint, confloat, field_validator, Field
 
 class Fruit:
     pass
+
+
 T = TypeVar('T', bound=Fruit)
 
 
@@ -29,17 +31,22 @@ class Banana(BaseModel, Fruit):
 
 class Box[T]:
     def __init__(self):
-        self.items: list[T] = list()
+        self.__items: list[T] = list()
 
     def add(self, item: T) -> None:
-        self.items.append(item)
+        self.__items.append(item)
 
     def remove(self, item: T) -> None:
-        self.items.remove(item)
+        self.__items.remove(item)
+
+    @property
+    def items(self):
+        return self.__items
+
 
 # Generic[T]
-class FruitBox(BaseModel, Generic[T] ):
-    fruit_type: T
+class FruitBox(BaseModel, Generic[T]):
+    # fruit_type: T
     pass
     # fruit_type: T
     # items: Field(default=list[T])
@@ -49,26 +56,29 @@ class BananaBox(Box[Banana]):
     pass
 
 
-# def main():
-#     apple_box = Box[Apple]()
-#     apple_box.add(Apple())
-#     apple_box.add(Apple())
-#     print(len(apple_box.items))
-#
-#     # banana_box = BananaBox()
-#     # banana_box.add(Banana())
-#     # print(len(apple_box.items))
-#     # print(type(apple_box))
-#     # items = apple_box.items
-#     # for row in items:
-#     #     print(f'{row=}')
-#
-#     # print(banana_box)
+def main():
+    apple_box = Box[Apple]()
+    apple_box.add(Apple())
+    apple_box.add(Apple())
+
+    for item in apple_box.items:
+        print(item.name)
+
+    banana_box = BananaBox()
+    banana_box.add(Banana())
+
+    # print(type(apple_box))
+    # items = apple_box.items
+    # for row in items:
+    #     print(f'{row=}')
+
+    # print(banana_box)
 
 
 if __name__ == '__main__':
-    fruit_box = FruitBox(fruit_type=Apple())
-    print(fruit_box.model_dump())
+    main()
+    # fruit_box = FruitBox(fruit_type=Apple())
+    # print(fruit_box.model_dump())
     # a: T = Apple()
     # print(a)
     # main()
