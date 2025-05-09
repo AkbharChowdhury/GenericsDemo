62847197cc81bbfec437c7b1812d7f34ffc9de12
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-05-09 08:41:40 +0100

    A

---
+++ bounded_fruit_generic.py
@@ -0,0 +1,32 @@
+from typing import Generic, TypeVar
+
+from fruits import *
+from pydantic import ConfigDict
+
+T = TypeVar('T', bound=Fruit)
+
+
+class MyBox(BaseModel, Generic[T]):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    FRUIT_TYPE: T
+    items: list[T] = []
+
+    def add(self, item: T):
+        self.model_validate({'FRUIT_TYPE': item})
+        self.items.append(item)
+
+
+def main():
+    fruit: T = Pear
+    fruit_list: list[Pear()] = [
+        Pear(),
+        # Apple(),
+    ]
+
+    box = MyBox[fruit](FRUIT_TYPE=fruit(), items=fruit_list)
+    box.add(Pear())
+
+
+if __name__ == '__main__':
+    main()
